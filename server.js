66737012b2026472604c9c34a0369c9c8d949e8a
daniel kafer <daniel.j.kafer@gmail.com>
2017-02-01 15:52:26 -0600

    added unirest call for images

--- server.js
+++ server.js
@@ -4,8 +4,23 @@ const app = express();
 
 app.use()
 
-app.get('/images', (req, res) => {
-	unirest.get('https://api.nasa.gov/planetary/earth/assets').end((results) => {
+app.get('/assets/:lat/:lon', (req, res) => {
+	let lat = req.params.lat
+	let lon = req.params.lon
+	let key = process.env.NASA_KEY
+	unirest.get('https://api.nasa.gov/planetary/earth/assets' + lat + lon + key)
+	.end( (results) => {
+		return results
+	})
+})
+
+app.get('/images/:lat/:lon/:date', (req, res) => {
+	let lat = req.params.lat;
+	let lon = req.params.lon;
+	let date = req.params.date;
+	let key = process.env.NASA_KEY
+	unirest.get('https://api.nasa.gov/planetary/earth/imagery' + lat + lon + date + key)
+	.end( (results) => {
 		return results
 	})
 })
