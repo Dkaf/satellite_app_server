df5da30fc14e85b3bf1f40021af7f34d49709697
daniel kafer <daniel.j.kafer@gmail.com>
2017-02-03 19:02:27 -0600

    changed image endpoint to return only body

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get('/images/:lat/:lon/:date', (req, res) => {
 	let key = process.env.NASA_KEY
 	unirest.get('https://api.nasa.gov/planetary/earth/imagery?' + "lat=" + lat + "&lon=" + lon + "&date=" + date + "&api_key=" + key)
 	.end( (results) => {
-		return res.json(results)
+		return res.json(results.body)
 	})
 })
 
